3ea7e18ad7a10b6bcb039d0eb50de6fa0804aaf8
Lovi <1442643087@qq.com>
2025-01-27 16:26:41 +0800

    💥 feat(compiler): 初始化伙伴匹配系统并配置好导航栏

---
+++ src/views/HomeView.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+import TheWelcome from '../components/TheWelcome.vue'
+import LayoutView from "@/views/layouts/LayoutView.vue";
+</script>
+
+<template>
+
+  <main>
+      <layout-view></layout-view>
+  </main>
+
+</template>
---
+++ src/views/layouts/LayoutView.vue
@@ -0,0 +1,32 @@
+<script setup lang="ts">
+import { ref } from 'vue';
+import { showToast } from 'vant';
+
+
+const active = ref(0);
+const onChange = (index) => showToast(`标签 ${index}`);
+
+</script>
+
+<template>
+    <!--顶部导航栏-->
+    <van-nav-bar title="标题" left-text="返回" left-arrow>
+        <template #right>
+            <van-icon name="search" size="18" />
+        </template>
+    </van-nav-bar>
+    <!--底部导航栏-->
+    <van-tabbar v-model="active" @change="onChange">
+        <van-tabbar-item icon="home-o">标签 1</van-tabbar-item>
+        <van-tabbar-item icon="search">标签 2</van-tabbar-item>
+        <van-tabbar-item icon="friends-o">标签 3</van-tabbar-item>
+        <van-tabbar-item icon="setting-o">标签 4</van-tabbar-item>
+    </van-tabbar>
+
+
+
+</template>
+
+<style scoped>
+
+</style>
